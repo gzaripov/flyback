3ffcc2297820a4e9de48eb934c30a94a49066b28
gzaripov <grigory.zaripov@gmail.com>
2019-04-25 23:30:15 +0300

    chore: copy needed files to dist folder to simplify publishing

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 const commonjs = require('rollup-plugin-commonjs');
 const resolve = require('rollup-plugin-node-resolve');
 const babel = require('rollup-plugin-babel');
+const copy = require('rollup-plugin-copy');
 const pkg = require('./package.json');
 
 const extensions = ['.js', '.ts'];
@@ -15,6 +16,9 @@ export default {
       runtimeHelpers: true,
       include: ['src/**/*'],
       exclude: 'node_modules/**',
+    }),
+    copy({
+      targets: ['CHANGELOG.md', 'LICENSE.md', 'README.md', 'package.json'],
     }),
   ],
   external: ['fs', 'http', 'https', 'path', 'url', 'stream', 'zlib'],
